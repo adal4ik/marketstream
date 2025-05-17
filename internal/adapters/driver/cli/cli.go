e94b35fa286107f08d3998816c4d533da87b0bcb
arabayev <adilet.adilek@gmail.com>
2025-05-17 19:09:00 +0500

    feat(driver): help added in cli

--- internal/adapters/driver/cli/cli.go
+++ internal/adapters/driver/cli/cli.go
@@ -1,1 +1,38 @@
 package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	port = flag.String("port", "8080", "Port number.")
+	help = flag.Bool("help", false, "Show this screen.")
+	Port string
+)
+
+func init() {
+	flag.Parse()
+
+	if *help {
+		fmt.Println(`
+		
+Usage:
+	marketstream [--port <N>]
+	marketstream --help
+
+Options:
+	--port N     Port number`)
+		os.Exit(0)
+	}
+
+	n, err := strconv.Atoi(*port)
+	if err != nil || n <= 0 || n > 65535 {
+		fmt.Fprintf(os.Stderr, "Error: invalid port number '%s'\n", *port)
+		os.Exit(1)
+	}
+
+	Port = fmt.Sprintf(":%d", n)
+}
