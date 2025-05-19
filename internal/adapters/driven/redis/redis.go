89dfd653333868a63cfbd3d0b3fa470c25ff7695
arabayev <adilet.adilek@gmail.com>
2025-05-19 14:16:28 +0500

    feat(redis): add connect to redis

--- internal/adapters/driven/redis/redis.go
+++ internal/adapters/driven/redis/redis.go
@@ -1,1 +1,22 @@
 package redis
+
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedis(ctx context.Context) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	pong, err := client.Ping(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Redis connection established:", pong)
+	return client
+}
